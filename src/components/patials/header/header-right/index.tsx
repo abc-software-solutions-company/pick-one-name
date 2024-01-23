a8d9c07e995936e6fefff564c1768c2b8e36272b
TonyHoa <91834973+79-Z1@users.noreply.github.com>
2024-01-23 16:23:59 +0700

    [PON-19] Làm mobie sidebar, responsive popup chúc mừng mobile (#16)
    * [PON-19] Làm mobie sidebar, responsive popup chúc mừng mobile
    
    * [PON-19] Làm mobie sidebar, responsive popup chúc mừng mobile - update gitignore
    
    * [PON-19] Làm mobie sidebar, responsive popup chúc mừng mobile  - Remove dist
    
    ---------
    
    Co-authored-by: TonyHoa <91834973+Hi-maybe@users.noreply.github.com>

--- src/components/patials/header/header-right/index.tsx
+++ src/components/patials/header/header-right/index.tsx
@@ -5,11 +5,11 @@ import Icon from '@/core-ui/icon';
 
 const HeaderRight: FC = () => {
   return (
-    <div className="flex grow items-center justify-end gap-6 text-[#FAFAFA] lg:grow-0">
-      <Button className="hidden items-center justify-center rounded-lg bg-blue-600 lg:flex lg:p-4">
+    <div className="flex grow items-center justify-end gap-1 text-[#FAFAFA] lg:grow-0 lg:gap-6">
+      <button className="flex items-center justify-center rounded bg-blue-600 p-2 lg:rounded-md lg:p-4">
         <Icon name="ico-volume-1" />
-      </Button>
-      <Button className="flex items-center justify-center rounded bg-blue-600 px-4 py-1 lg:rounded-lg lg:px-8 lg:py-4">
+      </button>
+      <Button className="flex items-center justify-center rounded bg-blue-600 px-4 py-2 lg:rounded-lg lg:px-8 lg:py-4">
         <span className="text-sm font-semibold leading-6 lg:text-lg">Đăng nhập</span>
       </Button>
     </div>
