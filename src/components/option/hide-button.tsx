8b086ecf4e91185f87caf3cdfa187a4940bce863
MaiAnhisreal <146809185+MaiAnhisreal@users.noreply.github.com>
2024-01-24 20:44:17 +0700

    Xử lý thao tác ẩn trong Option (#24)
    * [PON-21] Xử lý thao tác ẩn trong Option
    
    * Xử lý thao tác ẩn trong Option
    
    * Xử lý thao tác ẩn trong Option
    
    ---------
    
    Co-authored-by: TonyHoa <91834973+Hi-maybe@users.noreply.github.com>

--- src/components/option/hide-button.tsx
+++ src/components/option/hide-button.tsx
@@ -1,8 +1,12 @@
+import {useSetting} from '@/hooks/use-setting';
+
 const HideButton = () => {
+  const {setVisible} = useSetting();
   return (
     <button
       className=" flex items-center justify-center gap-2 py-2 px-3 text-sm text-black
       md:px-8 md:py-4 md:text-xl md:font-medium"
+      onClick={() => setVisible(false)}
     >
       Ẩn
     </button>
--- src/components/random-number/index.tsx
+++ src/components/random-number/index.tsx
@@ -1,17 +1,35 @@
+import cls from 'classnames';
 import React, {FC, useEffect} from 'react';
+
+import {useSetting} from '@/hooks/use-setting';
 
 import VerticalWheel from '../number-wheel';
 import RandomOption from '../option';
 
 const RandomNumber: FC = () => {
+  const {isVisible, setVisible} = useSetting();
+
   useEffect(() => {
     import(/* webpackChunkName: "vendor.lottie-player" */ '@lottiefiles/lottie-player');
   }, []);
 
   return (
-    <div className={'inline-flex h-full w-full flex-col gap-8 lg:flex lg:flex-row'}>
-      <VerticalWheel className={'max-h-[206px] w-full grow md:max-h-full lg:basis-2/3'} />
-      <RandomOption className="w-full grow lg:basis-1/3" />
+    <div
+      className={cls('inline-flex h-full w-full flex-col gap-8 lg:flex lg:flex-row', {
+        'relative justify-center': !isVisible
+      })}
+    >
+      <VerticalWheel className={'max-h-[206px] w-full md:max-h-full lg:basis-2/3'} />
+      {isVisible && <RandomOption className="w-full lg:basis-1/3" />}
+      {!isVisible && (
+        <button
+          className="-right-12 top-5 inline-flex h-fit max-w-[88px] items-center justify-center rounded-md 
+                  bg-blue-600 py-4 px-8 lg:absolute lg:-right-18 lg:-rotate-90 lg:rounded-tl-lg lg:rounded-tr-lg 3xl:-right-32"
+          onClick={() => setVisible(true)}
+        >
+          <p className="text-lg text-slate-50">Hiện</p>
+        </button>
+      )}
     </div>
   );
 };
