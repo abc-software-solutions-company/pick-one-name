281f8220947ac969005e7547821017ded89f331f
TonyHoa <91834973+Hi-maybe@users.noreply.github.com>
2024-01-25 09:48:32 +0700

    [PON-30] Thêm popup thông báo tính năng sẽ được cập nhật sau

--- src/components/patials/header/header-right/index.tsx
+++ src/components/patials/header/header-right/index.tsx
@@ -17,15 +17,18 @@ const HeaderRight: FC = () => {
   return (
     <div className="flex grow items-center justify-end gap-1 text-[#FAFAFA] md:gap-6 lg:grow-0">
       <button
-        className={cls('flex items-center justify-center rounded bg-blue-600 p-2 md:rounded-md md:p-4', {
-          'bg-gray-500': !isMusic
-        })}
+        className={cls(
+          'flex items-center justify-center rounded bg-blue-600 p-2 hover:bg-blue-700 md:rounded-md md:p-4',
+          {
+            'bg-gray-500': !isMusic
+          }
+        )}
         onClick={() => toggleMusic(!isMusic)}
       >
         {isMusic ? <Icon name="ico-volume-1" /> : <Icon name="ico-volume-x" />}
       </button>
       <Button
-        className="flex items-center justify-center rounded bg-blue-600 px-4 py-2 md:rounded-lg md:px-8 md:py-4"
+        className="flex items-center justify-center rounded bg-blue-600 px-4 py-2 hover:bg-blue-700 md:rounded-lg md:px-8 md:py-4"
         onClick={handleClick}
       >
         <span className="text-sm font-semibold leading-6 md:text-lg">Đăng nhập</span>
