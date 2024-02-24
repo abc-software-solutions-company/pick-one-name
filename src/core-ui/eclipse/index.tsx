b8c6930fc7f66759374c51473cbfb7c841e249e3
MaiAnhisreal <146809185+MaiAnhisreal@users.noreply.github.com>
2024-02-24 10:44:18 +0700

    Hoàn chỉnh UI trang Đăng Nhập (#47)
    Hoàn chỉnh UI trang Đăng Nhập

---
+++ src/core-ui/eclipse/index.tsx
@@ -0,0 +1,9 @@
+import {FC} from 'react';
+
+const Eclipse: FC = () => {
+  return (
+    <div className="h-32 w-32 rounded-full border-2 border-white bg-transparent md:h-96 md:w-96 lg:h-[700px] lg:w-[700px]"></div>
+  );
+};
+
+export default Eclipse;
